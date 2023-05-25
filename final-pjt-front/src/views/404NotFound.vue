b35234cd6d9772540d6dc30d83fc18c15ef4998e
song <yamcys@nate.com>
2023-05-25 09:58:59 +0900

    404

--- final-pjt-front/src/views/404NotFound.vue
+++ final-pjt-front/src/views/404NotFound.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="bg-gray">
-    <script type="text/javascript" src="snake.js"></script>
+    <!-- <script type="text/javascript" src="snake.js"></script>
     <div class="container">
       <h1>404</h1>
       <div id="score">Snake</div>
       <div id="plus"></div>
-    </div>
+    </div> -->
   </div>
 </template>
 

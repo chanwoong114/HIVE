b5af725ee504923174ee01cc60b87e5ffc12da60
chanwoong114 <cksdnd10001@gamil.com>
2023-05-25 14:05:11 +0900

    404 complete

--- final-pjt-front/src/views/404NotFound.vue
+++ final-pjt-front/src/views/404NotFound.vue
@@ -20,65 +20,243 @@
         <!-- Categories-->
         <h2 class="h4 text-center pb-lg-2 mb-4">Let's GAME!</h2>
         <div class="row g-4 pb-3 pb-lg-4 pb-xl-5">
-          <div class="col-md-4">
+          <div class="col-md-6" data-fancybox  data-src="#brick">
             <!-- Card-->
-            <div class="card zoom-effect h-100 border-0 rounded-1 overflow-hidden" style="min-height: 320px;"><span class="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-10 zindex-2"></span>
+            <div class="card zoom-effect h-50 border-0 rounded-1 overflow-hidden" style="min-height: 320px;">
+              <span class="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-10 zindex-2"></span>
               <div class="zoom-effect-wrapper rounded-1 position-absolute top-0 start-0 w-100 h-100">
-                <div class="zoom-effect-img bg-size-cover bg-position-bottom-center position-absolute top-0 start-0 w-100 h-100" style="background-image: url(assets/img/shop/categories/01.jpg);"></div>
-              </div><a class="card-body d-flex flex-column justify-content-end align-items-start position-absolute top-0 start-0 w-100 h-100 text-decoration-none zindex-3" href="shop-catalog.html"><span class="bg-light text-dark rounded-pill fs-sm fw-semibold lh-1 py-3 px-4">For the living room</span></a>
+                <div class="zoom-effect-img bg-size-cover bg-position-bottom-center position-absolute top-0 start-0 w-100 h-100" style="background-image: url(assets/img/404/brick.jpg);"></div>
+              </div>
             </div>
           </div>
-          <div class="col-md-8">
+          <div id="brick" style="display:none;width:70%; height: 80%;">
+            <canvas class="pt-5" id="myCanvas" width="480" height="320"></canvas>
+            <br>
+            <div class="d-flex justify-content-between" style="width: 30%;">
+              <button class="btn btn-success" style="width: 30%;"  @click="brickgamestart">게임 시작</button>
+              <button class="btn btn-danger" style="width: 30%;" @click="brickgameEnd" >게임 끝!</button>
+
+            </div>
+          </div>
+          <div class="col-md-6" data-fancybox  data-src="#twozero">
             <!-- Card-->
-            <div class="card zoom-effect border-0 rounded-1 overflow-hidden"><span class="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-10 zindex-2"></span>
-              <div class="zoom-effect-wrapper rounded-1"><img class="zoom-effect-img" src="assets/img/shop/categories/02.jpg" alt="Category image"></div><a class="card-body d-flex flex-column justify-content-end align-items-start position-absolute top-0 start-0 w-100 h-100 text-decoration-none zindex-3" href="shop-catalog.html"><span class="bg-light text-dark rounded-pill fs-sm fw-semibold lh-1 py-3 px-4">For the kitchen</span></a>
-            </div>
-            <div class="row g-4 pt-4">
-              <div class="col-sm-6">
-                <!-- Card-->
-                <div class="card zoom-effect border-0 rounded-1 overflow-hidden"><span class="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-10 zindex-2"></span>
-                  <div class="zoom-effect-wrapper rounded-1"><img class="zoom-effect-img" src="assets/img/shop/categories/03.jpg" alt="Category image"></div><a class="card-body d-flex flex-column justify-content-end align-items-start position-absolute top-0 start-0 w-100 h-100 text-decoration-none zindex-3" href="shop-catalog.html"><span class="bg-light text-dark rounded-pill fs-sm fw-semibold lh-1 py-3 px-4">For the bathroom</span></a>
-                </div>
-              </div>
-              <div class="col-sm-6">
-                <!-- Card-->
-                <div class="card zoom-effect border-0 rounded-1 overflow-hidden"><span class="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-10 zindex-2"></span>
-                  <div class="zoom-effect-wrapper rounded-1"><img class="zoom-effect-img" src="assets/img/shop/categories/04.jpg" alt="Category image"></div><a class="card-body d-flex flex-column justify-content-end align-items-start position-absolute top-0 start-0 w-100 h-100 text-decoration-none zindex-3" href="shop-catalog.html"><span class="bg-light text-dark rounded-pill fs-sm fw-semibold lh-1 py-3 px-4">For the bedroom</span></a>
-                </div>
+            <div class="card zoom-effect h-50 border-0 rounded-1 overflow-hidden" style="min-height: 320px;"><span class="position-absolute top-0 start-0 w-100 h-100 bg-dark opacity-10 zindex-2"></span>
+              <div class="zoom-effect-wrapper rounded-1 position-absolute top-0 start-0 w-100 h-100">
+                <div class="zoom-effect-img bg-size-cover bg-position-bottom-center position-absolute top-0 start-0 w-100 h-100" style="background-image: url(assets/img/404/snake.png);"></div>
               </div>
             </div>
+          </div>
+          <div id="twozero" style="display:none;width:70%; height: 80%;">
+            <canvas id="game" width="400" height="400"></canvas>
+            <br>
+            <div class="d-flex justify-content-between" style="width: 30%;">
+              <button class="btn btn-success" style="width: 30%;"  @click="gameStart">게임 시작</button>
+              <button class="btn btn-danger" style="width: 30%;" @click="gameEnd" >게임 끝!</button>
+
+            </div>
+
           </div>
         </div>
       </div>
     </main>
-    <!-- <div class="container">
-      <h1>404</h1>
-      <a class="personBox" href="" data-fancybox  data-src="#brick">
-        아랑랑
-      </a>
-      <div id="brick" style="display:none;width:70%; height: 80%;">
-        <canvas class="pt-5" id="myCanvas" width="480" height="320"></canvas>
-        <br>
-        <div class="d-flex justify-content-between" style="width: 30%;">
-          <button class="btn btn-success" style="width: 30%;"  @click="brickgamestart">게임 시작</button>
-          <button class="btn btn-danger" style="width: 30%;" @click="brickgameEnd" >게임 끝!</button>
-
-        </div>
-      </div>
-      <TwoZeroView />
-    </div> -->
+    
   </div>
 </template>
 
 <script>
-// import TwoZeroView from '@/components/game/TwoZeroView.vue'
 
 export default {
   name:"404NotFound",
-  components: {
-    // TwoZeroView
-  },
   methods: {
+    gameEnd() {
+      this.$router.go(this.$router.currentRoute)
+    },
+    gameStart() {
+      const canvas=document.getElementById('game');
+      const ctx=canvas.getContext('2d');
+      //increase snake size 
+      class snakePart{
+      constructor(x, y){
+          this.x=x;
+          this.y=y;
+      }
+
+      }
+
+      let speed=7;
+      let tileCount=20; 
+
+      let tileSize=canvas.clientWidth/tileCount-2;
+      let headX=10;
+      let headY=10;
+
+      // array for snake parts
+      const snakeParts=[];
+      let tailLength=2;
+
+      //initialize the speed of snake
+      let xvelocity=0;
+      let yvelocity=0;
+
+      //draw apple
+      let appleX=5;
+      let appleY=5;
+
+      //scores
+      let score=0;
+
+      // create game loop-to continously update screen
+      function drawGame(){
+          changeSnakePosition();
+          // game over logic
+          let result=isGameOver();
+          if(result){// if result is true
+              return;
+          }
+          clearScreen();
+          drawSnake();
+          drawApple();
+        
+          checkCollision()
+          drawScore();
+          setTimeout(drawGame, 1000/speed);//update screen 7 times a second
+      }
+      //Game Over function
+      function isGameOver(){
+          let gameOver=false; 
+          //check whether game has started
+          if(yvelocity===0 && xvelocity===0){
+              return false;
+          }
+          if(headX<0){//if snake hits left wall
+              gameOver=true;
+          }
+          else if(headX===tileCount){//if snake hits right wall
+              gameOver=true;
+          }
+          else if(headY<0){//if snake hits wall at the top
+              gameOver=true;
+          }
+          else if(headY===tileCount){//if snake hits wall at the bottom
+              gameOver=true;
+          }
+
+          //stop game when snake crush to its own body
+
+          for(let i=0; i<snakeParts.length;i++){
+              let part=snakeParts[i];
+              if(part.x===headX && part.y===headY){//check whether any part of snake is occupying the same space
+                  gameOver=true;
+                  break; // to break out of for loop
+              }
+          }
+          
+
+          //display text Game Over
+          if(gameOver){
+          ctx.fillStyle="white";
+          ctx.font="50px verdana";
+          ctx.fillText("Game Over! ", canvas.clientWidth/6.5, canvas.clientHeight/2);//position our text in center
+          }
+
+          return gameOver;// this will stop execution of drawgame method
+      }
+
+      // score function
+      function drawScore(){
+      ctx.fillStyle="white"// set our text color to white
+      ctx.font="10px verdena"//set font size to 10px of font family verdena
+      ctx.fillText("Score: " +score, canvas.clientWidth-50,10);// position our score at right hand corner 
+
+      }
+
+      // clear our screen
+      function clearScreen(){
+
+      ctx.fillStyle= 'black'// make screen black
+      ctx.fillRect(0,0,canvas.clientWidth,canvas.clientHeight)// black color start from 0px left, right to canvas width and canvas height
+
+      }
+      function drawSnake(){
+          
+          ctx.fillStyle="green";
+          //loop through our snakeparts array
+          for(let i=0;i<snakeParts.length;i++){
+              //draw snake parts
+              let part=snakeParts[i]
+              ctx.fillRect(part.x *tileCount, part.y *tileCount, tileSize,tileSize)
+          }
+          //add parts to snake --through push
+          snakeParts.push(new snakePart(headX,headY));//put item at the end of list next to the head
+          if(snakeParts.length>tailLength){
+              snakeParts.shift();//remove furthest item from  snake part if we have more than our tail size
+
+          }
+          ctx.fillStyle="orange";
+          ctx.fillRect(headX* tileCount,headY* tileCount, tileSize,tileSize)
+
+
+      }
+      function changeSnakePosition(){
+          headX=headX + xvelocity;
+          headY=headY+ yvelocity;
+          
+      }
+      function drawApple(){
+          ctx.fillStyle="red";
+          ctx.fillRect(appleX*tileCount, appleY*tileCount, tileSize, tileSize)
+      }
+      // check for collision and change apple position
+      function checkCollision(){
+          if(appleX==headX && appleY==headY){
+              appleX=Math.floor(Math.random()*tileCount);
+              appleY=Math.floor(Math.random()*tileCount);
+              tailLength++;
+              score++; //increase our score value
+
+          }
+      }
+      //add event listener to our body
+      document.body.addEventListener('keydown', keyDown);
+
+      function keyDown()
+      //up
+      {
+          if(event.keyCode==38){
+              //prevent snake from moving in opposite direcction
+              if(yvelocity==1)
+              return;
+              yvelocity=-1;
+              xvelocity=0;
+              
+          }
+          //down
+          if(event.keyCode==40){
+              if(yvelocity==-1)
+              return;
+              yvelocity=1;
+              xvelocity=0;
+          }
+
+      //left
+          if(event.keyCode==37){
+              if(xvelocity==1)
+              return;
+              yvelocity=0;
+              xvelocity=-1;
+          }
+          //right
+          if(event.keyCode==39){
+              if(xvelocity==-1)
+              return;
+              yvelocity=0;
+              xvelocity=1;
+          }
+      }
+
+      drawGame();
+
+    },
     brickgamestart() {
       var canvas = document.getElementById("myCanvas");
       var ctx = canvas.getContext("2d");
@@ -264,5 +442,18 @@ export default {
     flex-direction: column; /* arrage items on top of the other */
     justify-content: center;
     align-items: center;
+  }
+
+  #twozero{
+  margin: 0px;
+  padding: 0px;
+  display: flex;
+  flex-direction: column; /* arrage items on top of the other */
+  justify-content: center;
+  align-items: center;
+  }
+  canvas{
+  border: 1px solid black; /*elevate our canvas and add shadow*/
+
   }
 </style>
